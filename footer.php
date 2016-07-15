cb47091e17b34549d490c7adbd63c3af48fab6e3
Lauren Pittenger <lepittenger@gmail.com>
2016-07-15 14:25:15 -0400

    Removed sass, added CSS

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'psd-to-wp-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'psd-to-wp-theme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'psd-to-wp-theme' ), 'psd-to-wp-theme', '<a href="http://underscores.me/" rel="designer">Lauren Pittenger</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'psd-to-wp-theme' ), 'psd-to-wp-theme', '<a href="http://laurenpittenger.com" rel="designer">Lauren Pittenger</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
