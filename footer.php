f65f97c14f8e556b9a119b21b513eafec93dc3d0
Lauren Pittenger <lepittenger@gmail.com>
2016-07-15 13:52:24 -0400

    Initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package PSD_to_WP_Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'psd-to-wp-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'psd-to-wp-theme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'psd-to-wp-theme' ), 'psd-to-wp-theme', '<a href="http://underscores.me/" rel="designer">Lauren Pittenger</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
